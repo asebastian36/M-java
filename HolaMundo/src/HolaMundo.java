40f1dd448475613e7c4956468a08764d96b0ab3d
asebastian36 <asebastianjob@gmail.com>
2023-07-09 12:47:48 -0600

    finalice el proyecto de HolaMundo

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,8 @@
+import tarea.Prueba;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Intellij IDEA");
+        Prueba.mostrarNombre("Angel Sebastian Franco Lopez");
+    }
+}
---
+++ HolaMundo/src/tarea/Prueba.java
@@ -0,0 +1,9 @@
+package tarea;
+
+public class Prueba {
+    //  mostrar en consola mi nombre:
+
+    public static void mostrarNombre(String nombre) {
+        System.out.println("nombre = " + nombre);
+    }
+}
